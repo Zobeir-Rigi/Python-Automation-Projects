125e8f4b5515e8b91c3425870a3f181c1129977b
zobeir-rigi <rigi.zobir@yahoo.com>
2024-01-23 18:53:28 +0000

    created a function to get some specific data from the API

--- currency.py
+++ currency.py
@@ -1,9 +1,23 @@
 import requests
 API_KEY = 'fca_live_WaNVuedUnJPSFHwMvmzoytJdrXiHhDYebZJQEo9N'  #capital, because it's a const.
-BASE_URl = f'https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}' #Because of Authorization. It's $ instead of f in JS.
+BASE_URL = f'https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}' #Because of Authorization. It's $ instead of f in JS.
 
 
+CURRENCIES = ["USD", "CAD", "EUR", "AUD", "CNY"]
 
+def convert_currency(base): 
+    currencies = ",".join(CURRENCIES) #In JS => const currencies = CURRENCIES.join(',');
+    url = f"{BASE_URL}&base_currency={base}&currencies={currencies}"
+   #try catch in JS, accessing data by fetching
+    try: 
+        response = requests.get(url)
+        data = response.json()
+        print (data)
+    except:
+        print("Invalid currency.")
+        return None
+# defining a function to format the url
+convert_currency("CAD")
 
 
 
