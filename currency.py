ab286316e0cb77bb41b3aee8bdae467661aabf3a
zobeir-rigi <rigi.zobir@yahoo.com>
2024-01-23 18:27:28 +0000

    now we have our API_KEY and BASE_URL from the website

--- currency.py
+++ currency.py
@@ -1,1 +1,9 @@
 import requests
+API_KEY = 'fca_live_WaNVuedUnJPSFHwMvmzoytJdrXiHhDYebZJQEo9N'  #capital, because it's a const.
+BASE_URl = f'https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}' #Because of Authorization. It's $ instead of f in JS.
+
+
+
+
+
+
